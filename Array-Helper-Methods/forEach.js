3813941396e1428b86a9ad80235ced4da2bab69f
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-26 10:59:29 +0530

    for each helper

--- Array-Helper-Methods/forEach.js
+++ Array-Helper-Methods/forEach.js
@@ -1,9 +1,22 @@
-let numbers = [1,2,3,4,5];
+// classic for loop
+let colors = ['red', 'blue', 'green'];
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+// es6 forEach
+colors.forEach((color) => {
+  console.log(color);
+});
+
+// example 2
+let numbers = [1, 2, 3, 4, 5];
 
 let sum = 0;
 
 numbers.forEach((number) => {
-    sum += number
-})
+  sum += number;
+});
 
 console.log(sum);
