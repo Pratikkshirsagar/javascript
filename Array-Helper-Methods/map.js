e4212b5333711dc0d6604390bd301777a4bc1ca9
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-07-27 12:30:49 +0530

    map helper

---
+++ Array-Helper-Methods/map.js
@@ -0,0 +1,17 @@
+// classic for loop
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  doubledNumbers.push(numbers[i] * 2);
+}
+
+console.log(doubledNumbers);
+
+// Map
+
+const doubled = numbers.map((number) => {
+  return number * 2;
+});
+
+console.log(doubled);
